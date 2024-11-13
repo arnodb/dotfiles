66db359fc26d7c64183854329d3b8712298e0b6e
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2024-11-13 21:56:00 +0100

    More telescope config

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -4,5 +4,5 @@ nmap('<C-P>', '<cmd>Telescope find_files<CR>')
 
 nmap('<leader>fb', '<cmd>Telescope buffers<CR>')
 nmap('<leader>fs', '<cmd>Telescope lsp_workspace_symbols<CR>')
---nmap('<leader>fd', '<cmd>Telescope diagnostics<CR>')
---nmap('gr', '<cmd>Telescope lsp_references<CR>')
+nmap('<leader>fd', '<cmd>Telescope diagnostics<CR>')
+nmap('<leader>fr', '<cmd>Telescope lsp_references<CR>')
