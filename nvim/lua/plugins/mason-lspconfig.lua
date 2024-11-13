5bb7f68449d4f9ba0cd298e8110b491cdf3a044a
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2024-11-13 21:20:28 +0100

    Add mason and mason-lspconfig

---
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require 'config.mason-lspconfig'
+    end,
+  },
+}
---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require 'config.mason'
+    end,
+  },
+}
